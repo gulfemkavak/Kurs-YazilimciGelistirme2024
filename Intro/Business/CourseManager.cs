26c3279c07b443f0d3ac7e82436cc2254f0c4554
Gulfem Kavak <gulfemkavak@gmail.com>
2024-03-03 20:59:38 +0300

    Day 2 Lesson 2 Commit

--- Intro/Business/CourseManager.cs
+++ Intro/Business/CourseManager.cs
@@ -1,4 +1,5 @@
-﻿using Intro.Entities;
+﻿using Intro.DataAccess.Concretes;
+using Intro.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,36 +10,12 @@ namespace Intro.Business;
 
 public class CourseManager
 {
-    //constructor
- 
-    Courses[] courses=new Courses[3];
-    public CourseManager()
+
+        public List<Courses> GetAll()
     {
-        Courses course1 = new Courses();
-        course1.Id = 1;
-        course1.Name = "C#";
-        course1.Description = ".NET 8 vs.";
-        course1.Price = 100;
-        Courses course2 = new Courses();
-        course2.Id = 2;
-        course2.Name = "Java";
-        course2.Price = 100;
-        Courses course3 = new Courses();
-        course3.Id = 3;
-        course3.Name = "Python";
-        course3.Description = "Python 3.12";
-        course3.Price = 150;
-        courses[0]=course1;
-        courses[1]=course2;
-        courses[2]=course3;
+            //business rules
+            CourseDal courseDal = new CourseDal();
 
-    }
-
-    public Courses[] GetAll()
-    {
-
-      return courses;
-
-
+             return courseDal.GetAll();
     }
 }
---
+++ Intro/DataAccess/Concretes/CourseDal.cs
@@ -0,0 +1,46 @@
+﻿using Intro.Business;
+using Intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.DataAccess.Concretes;
+
+public class CourseDal//DataAccessLayer/repository
+{
+    List<Courses> courses;
+    public CourseDal()
+    {
+            Courses course1 = new Courses();
+            course1.Id = 1;
+            course1.Name = "C#";
+            course1.Description = ".NET 8 vs.";
+            course1.Price = 100;
+            Courses course2 = new Courses();
+            course2.Id = 2;
+            course2.Name = "Java";
+            course2.Price = 100;
+            Courses course3 = new Courses();
+            course3.Id = 3;
+            course3.Name = "Python";
+            course3.Description = "Python 3.12";
+            course3.Price = 150;
+
+            courses= new List<Courses> { course1, course2, course3 };
+        
+    }
+    public List<Courses> GetAll()
+
+    {
+        //Burada db işlemleri yapılır
+        return courses;
+    }
+
+    public void Add(Courses course)
+    {
+        courses.Add(course);
+    }
+
+}
