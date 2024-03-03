d57f2149d86180027ad421ed15d6b2026ac583bd
Gulfem Kavak <gulfemkavak@gmail.com>
2024-03-03 14:18:42 +0300

    Add project files.

---
+++ Intro/Business/CourseManager.cs
@@ -0,0 +1,44 @@
+﻿using Intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.Business;
+
+public class CourseManager
+{
+    //constructor
+    //newi gördüğünde çalışan bir blok var 
+    Course[] courses=new Course[3];
+    public CourseManager()
+    {
+        Course course1 = new Course();
+        course1.Id = 1;
+        course1.Name = "C#";
+        course1.Description = ".NET 8 vs.";
+        course1.Price = 100;
+        Course course2 = new Course();
+        course2.Id = 2;
+        course2.Name = "Java";
+        course2.Price = 100;
+        Course course3 = new Course();
+        course3.Id = 3;
+        course3.Name = "Python";
+        course3.Description = "Python 3.12";
+        course3.Price = 150;
+        courses[0]=course1;
+        courses[1]=course2;
+        courses[2]=course3;
+
+    }
+
+    public Course[] GetAll()
+    {
+
+      return courses;
+
+
+    }
+}
---
+++ Intro/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using Intro.Business;
+using Intro.Entities;
+
+Console.WriteLine("Hello, World!");
+
+          string message1 = "Krediler";
+          int term = 12;
+          double amount = 100000;
+bool isAuthenticated = true;
+          //variables-değişkenler-->camelCase yazılır.       bool isAuthenticated = false;
+  Console.WriteLine(message1);
+  Console.WriteLine(term);
+//condition
+
+if (isAuthenticated)
+
+{
+    Console.WriteLine("Buton-->Hoşgeldin Engin");
+}
+else
+{ 
+    Console.WriteLine("Buton-->Sisteme giriş yap.");
+}
+
+string[] loans = { "Kredi1", "Kredi2", "Kredi3","Kredi4","Kredi5","Kredi6" };//normalde db den gelecek
+//string[] loans2 = new string[] 
+//{
+//};
+//string[] loans3 = new string[6];
+//loans2[0]= "Kredi 1";
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine($"{loans[i]} {amount}");
+    Console.WriteLine(loans[i] );
+}
+
+
+CourseManager courseManager = new();
+
+Course[] courses2=courseManager.GetAll();
+for (int i = 0; i < courses2.Length; i++)
+{
+    Console.WriteLine(courses2[i].Name + "/" + courses2[i].Price);
+}
+
+
+Console.WriteLine("Kod Bitti.");
